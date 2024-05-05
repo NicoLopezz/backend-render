9573add4413b060d84b6d0df819a22a817dd6918
LOPEZ NICOLAS <nicolas.lopez1919@gmail.com>
2024-05-05 11:13:31 -0300

    changed fetch

--- public/login.js
+++ public/login.js
@@ -7,7 +7,7 @@ formElement.addEventListener("submit", async(event) => {
     let email = document.getElementById("email").value
     let datosUsuario = {Pass : password , Email : email}
     let datosUsuarioJson = JSON.stringify(datosUsuario);
-    const res = await fetch("http://localhost:10001/api/login", {     
+    const res = await fetch("https://backend-render-test-se07.onrender.com/api/login", {     
         method: 'POST',
             headers:
             {
