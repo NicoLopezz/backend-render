2a4f330fa9e50077748bbb7da5bbf6f1c575caa6
nikov1992 <nicolas.lopez1919@gmail.com>
2024-07-27 16:03:04 -0300

    fix pinging

--- src/pinging.js
+++ src/pinging.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 
-export const INTERVALO_PETICIONES = 5 * 1000; // 5 minutos en milisegundos
+export const INTERVALO_PETICIONES = 45 * 1000; // 5 minutos en milisegundos
 
 setInterval(async () => {
   try {
     // Realizar una petición GET a tu servidor en Render
-    const response = await fetch('https://backend-render-bdaf.onrender.com/');
+    const response = await fetch('https://backend-render-main.onrender.com/');
     console.log('Respuesta del servidor:', response.statusText);
   } catch (error) {
     console.error('Error al realizar la petición:', error);
