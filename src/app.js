0ba22f011ac9a7fab78e15da552957ed3774f0d8
Nicolas <nicolas.lopez1919@gmail.com>
2025-05-29 22:48:27 -0300

    able cors

--- src/app.js
+++ src/app.js
@@ -10,9 +10,10 @@ app.set('port' , process.env.PORT || 10001);
 app.use(express.json());
 app.use(express.text()); 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://www.oxygentoken.org"],
     credentials: true,
 }));
+
 app.use("/api" , AuthRoutes); 
 app.use(express.static("public"))
 
